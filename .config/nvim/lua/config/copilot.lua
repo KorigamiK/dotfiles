7b95435298b76ef8883562122ade3617e961ee2a
korigamik <korigamik@gmail.com>
2022-11-28 21:12:12 +0530

    fix completion

--- .config/nvim/lua/config/copilot.lua
+++ .config/nvim/lua/config/copilot.lua
@@ -4,6 +4,9 @@ function M.setup()
 	vim.g.copilot_node_command = "/home/korigamik/.nvm/versions/node/v16.15.0/bin/node"
 	vim.g.copilot_no_tab_map = true
 	vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+	vim.g.copilot_filetypes = { ["*"] = true, ["xml"] = false, ["markdown"] = true }
+	-- explicitly request the completion
+	vim.api.nvim_set_keymap("i", "<C-k>", "copilot#Complete()", { silent = true, expr = true })
 end
 
 return M
