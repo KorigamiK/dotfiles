b7f29fa6f24262d19c6dc67c54c9a9e037a6bf67
korigamik <korigamik@gmail.com>
2022-11-28 12:22:43 +0530

    changes

--- .config/nvim/lua/config/autopairs.lua
+++ .config/nvim/lua/config/autopairs.lua
@@ -1,11 +1,9 @@
 local M = {}
 
 function M.setup()
-    local npairs = require "nvim-autopairs"
-    -- allow autopairs in all files
-    npairs.setup {
-
-    }
+	local npairs = require("nvim-autopairs")
+	-- allow autopairs in all files
+	npairs.setup({})
 end
 
 return M
