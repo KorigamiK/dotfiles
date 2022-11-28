6720051fb1403c1703fc46da506ce7d3e57f7655
korigamik <korigamik@gmail.com>
2022-11-28 12:22:43 +0530

    changes

--- .config/nvim/lua/config/copilot.lua
+++ .config/nvim/lua/config/copilot.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 function M.setup()
-    vim.g.copilot_node_command = "/home/korigamik/.nvm/versions/node/v16.15.0/bin/node"
-    vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept(\"<CR>\")", { silent = true, expr = true })
+	vim.g.copilot_node_command = "/home/korigamik/.nvm/versions/node/v16.15.0/bin/node"
+	vim.g.copilot_no_tab_map = true
+	vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 end
 
 return M
