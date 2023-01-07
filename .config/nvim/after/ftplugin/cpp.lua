377b239f118f388f5738f1e44264d3dd73110a92
korigamik <korigamik@gmail.com>
2023-01-07 15:22:12 +0530

    updates and fixes

--- .config/nvim/after/ftplugin/cpp.lua
+++ .config/nvim/after/ftplugin/cpp.lua
@@ -1,16 +1,16 @@
-local wk = require("which-key")
-local opts = {
-	mode = "n",
-	prefix = "<leader>",
-	buffer = nil,
-	silent = false,
-	noremap = true,
-	nowait = true,
-}
-local mappings = {
-	["r"] = {
-		name = "Run",
-		c = { ":!cmake --build build/linux", "cmake build" },
-	},
-}
-wk.register(mappings, opts)
+-- local wk = require("which-key")
+-- local opts = {
+-- 	mode = "n",
+-- 	prefix = "<leader>",
+-- 	buffer = nil,
+-- 	silent = false,
+-- 	noremap = true,
+-- 	nowait = true,
+-- }
+-- local mappings = {
+-- 	["r"] = {
+-- 		name = "Run",
+-- 		c = { ":!cmake --build build/linux", "cmake build" },
+-- 	},
+-- }
+-- wk.register(mappings, opts)
--- .config/nvim/lua/config/copilot.lua
+++ .config/nvim/lua/config/copilot.lua
@@ -4,7 +4,12 @@ function M.setup()
 	vim.g.copilot_node_command = "/home/korigamik/.nvm/versions/node/v16.15.0/bin/node"
 	vim.g.copilot_no_tab_map = true
 	vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-	vim.g.copilot_filetypes = { ["*"] = true, ["xml"] = false, ["markdown"] = true }
+	vim.g.copilot_filetypes = {
+		["*"] = true,
+		["xml"] = false,
+		["markdown"] = true,
+		["*.env*"] = false, -- disable copilot for .env files
+	}
 	-- explicitly request the completion
 	vim.api.nvim_set_keymap("i", "<C-k>", "copilot#Complete()", { silent = true, expr = true })
 end
