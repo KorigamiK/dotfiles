7a1eb1b31265dd927eaf70e1da2c4de2ebc5cb1a
korigamik <korigamik@gmail.com>
2023-05-12 17:14:55 +0530

    remove old config

--- .config/nvim/lua/config/dap/python.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-
-function M.setup() require('dap-python').setup('~/miniconda3/bin/python') end
-
-return M
-
--- .config/nvim/lua/config/lsp/arduino_language_server.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.setup(arduino_language_server)
-	return {
-		cmd = { "arduino-language-server", "--stdio" },
-	}
-end
-
-return M
