aea67fe3144cb432b73d9faf1d3151fbbb98b9e7
korigamik-hypr <korigamik@gmail.com>
2023-06-18 19:36:26 +0530

    upadates

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "palenight",
+  theme = "nightfox",
 
   hl_override = highlights.override,
   hl_add = highlights.add,
