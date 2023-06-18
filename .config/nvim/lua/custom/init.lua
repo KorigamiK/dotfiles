a77f454253d1ebc6d76bbb91792133e46b4c0edf
korigamik-hypr <korigamik@gmail.com>
2023-06-19 03:14:50 +0530

    add snippets, update config

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -6,8 +6,8 @@
 --   command = "tabdo wincmd =",
 -- })
 
-vim.g.snipmate_snippets_path = "~/Dev/projects/snippets/snipmate"
-vim.g.vscode_snippets_path = "~/Dev/projects/snippets/vscode"
+vim.g.snipmate_snippets_path = "~/Dev/projects/dotfiles/snippets/snipmate"
+vim.g.vscode_snippets_path = "~/Dev/projects/dotfiles/snippets/vscode"
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, { callback = function() vim.highlight.on_yank() end })
 
