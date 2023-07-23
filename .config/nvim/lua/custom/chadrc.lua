264f251a0482ffee7bb4c0bec2adf3af3220ad9b
korigamik-hypr <korigamik@gmail.com>
2023-07-24 02:21:42 +0530

    add astro configs

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "ayu_dark",
+  theme = "doomchad",
 
   hl_override = highlights.override,
   hl_add = highlights.add,
