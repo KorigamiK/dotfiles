0d1dfb4fced41b32fba5b2a78ec1f63682ad16d8
korigamik-hypr <korigamik@gmail.com>
2023-08-23 12:01:54 +0530

    add zen mode bindings

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -9,12 +9,21 @@
 vim.g.snipmate_snippets_path = "~/Dev/projects/dotfiles/snippets/snipmate"
 vim.g.vscode_snippets_path = "~/Dev/projects/dotfiles/snippets/vscode"
 
-vim.api.nvim_create_autocmd({ "TextYankPost" }, { callback = function() vim.highlight.on_yank() end })
+vim.api.nvim_create_autocmd({ "TextYankPost" }, { callback = function() vim.highlight.on_yank() end, })
 
 vim.opt.foldlevel = 20
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 vim.g.markdown_fenced_languages = {
-  "ts=typescript"
+  "ts=typescript",
 }
+
+function ToggleTabline()
+  print(vim.opt.showtabline)
+  if vim.opt.showtabline._value == 0 then
+    vim.opt.showtabline = 2
+  else
+    vim.opt.showtabline = 0
+  end
+end
