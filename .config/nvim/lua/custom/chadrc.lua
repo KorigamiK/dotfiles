a969f56be87152adca5cc6aabf59f03981feaadf
korigamik-hypr <korigamik@gmail.com>
2023-10-09 02:08:00 +0530

    add zenmode

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "bearded-arc",
+  theme = "catppuccin",
   lsp_semantic_tokens = true,
 
   hl_override = highlights.override,
