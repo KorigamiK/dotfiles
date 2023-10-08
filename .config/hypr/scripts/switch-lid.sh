60d0328f40173aa104006fa21866dd6218c286a2
korigamik-hypr <korigamik@gmail.com>
2023-10-09 01:50:29 +0530

    apply shell check to scripts

--- .config/hypr/scripts/switch-lid.sh
+++ .config/hypr/scripts/switch-lid.sh
@@ -3,7 +3,7 @@
 if grep open /proc/acpi/button/lid/LID/state; then
     hyprctl keyword monitor "eDP-1, preferred, auto, 1"
 else
-    if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
+    if [[ $(hyprctl monitors | grep -c "Monitor") != 1 ]]; then
         hyprctl keyword monitor "eDP-1, disable"
     fi
 fi
