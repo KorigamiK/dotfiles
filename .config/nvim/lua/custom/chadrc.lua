445b79778586d6a109aae2dc71279aa0f55a9616
korigamik-hypr <korigamik@gmail.com>
2023-10-14 17:48:31 +0530

    update nvim config

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "catppuccin",
+  theme = "palenight",
   lsp_semantic_tokens = true,
 
   hl_override = highlights.override,
