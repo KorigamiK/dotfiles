d936f83f602510e491bfa6d0a2b27d0791161282
korigamik-hypr <korigamik@gmail.com>
2023-10-26 20:20:25 +0530

    fix rust analyzer lsp config

--- .config/nvim/lua/custom/configs/rust-tools.lua
+++ .config/nvim/lua/custom/configs/rust-tools.lua
@@ -1,15 +1,44 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local options = {
+return {
   server = {
     on_attach = on_attach,
     capabilities = capabilities,
+    settings = {
+      ["rust-analyzer"] = {
+        completion = {
+          addCallArgumentSnippets = true,
+          addCallParenthesis = true,
+          enableExperimental = true,
+          autoimport = { enable = true },
+          postfix = { enable = false, true },
+        },
+        rustfmt = {
+          extraArgs = {
+            "--config",
+            "imports_granularity=Crate,wrap_comments=true,comment_width=100,max_width=100,tab_spaces=2",
+          },
+        },
+        cargo = {
+          autoReload = true,
+          loadOutDirsFromCheck = true,
+          allFeatures = true,
+        },
+        checkOnsave = {
+          enable = true,
+          allFeatures = true,
+        },
+        hoverActions = {
+          enable = true,
+          references = true,
+        },
+        procMacro = { enable = true },
+        inlayHints = { enable = true },
+        lens = { enable = true },
+        lruCapacity = 250,
+        workspace = { symbol = { search = { kind = "all_symbols" } } },
+      },
+    },
   },
 }
-
-local function setup()
-  require("rust-tools").setup(options)
-end
-
-return { setup = setup }
