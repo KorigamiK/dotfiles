14b15441792dd5d365c875c015f993e6a1c22b74
korigamik-hypr <korigamik@gmail.com>
2023-10-26 20:20:25 +0530

    fix rust analyzer lsp config

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -175,8 +175,11 @@ local plugins = {
     "simrat39/rust-tools.nvim",
     ft = "rust",
     dependencies = "neovim/nvim-lspconfig",
-    config = function()
-      require("custom.configs.rust-tools").setup()
+    opts = function()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
     end,
   },
 
