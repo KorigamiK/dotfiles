da49bd97480d039e57c8c58b9b9b475a50fe1967
korigamik-hypr <korigamik@gmail.com>
2023-11-04 12:51:05 +0530

    gotta love this font

--- fix-mpv-cursor.sh
+++ fix-mpv-cursor.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 default_user_icons_folder="$HOME/.icons/default"
-mkdir -p "${default_user_icons_folder}" 2> /dev/null
+mkdir -p "${default_user_icons_folder}" 2>/dev/null
 #cursor_theme_name=$(gsettings get org.gnome.desktop.interface cursor-theme | cut -d "'" -f 2)
 cursor_theme_name=$(dconf read /org/gnome/desktop/interface/cursor-theme | cut -d "'" -f 2)
-cursor_theme_location=$(find / -name "${cursor_theme_name}" 2> /dev/null | head -n 1)
+cursor_theme_location=$(find / -name "${cursor_theme_name}" 2>/dev/null | head -n 1)
 cursor_icons_location="${cursor_theme_location}/cursors"
 ln -nfs "${cursor_icons_location}" "${default_user_icons_folder}/"
