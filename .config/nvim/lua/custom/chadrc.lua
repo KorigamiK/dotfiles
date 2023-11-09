f6a3cbcbd7d9b3dc8ee40c903e4f380c92cc64e0
korigamik-hypr <korigamik@gmail.com>
2023-11-10 02:43:16 +0530

    fix clock

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "bearded-arc",
+  theme = "chadracula",
   lsp_semantic_tokens = true,
   extended_integrations = { "trouble" },
 
