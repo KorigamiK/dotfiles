b96f19e7bee98c85f45ca37e6eb9ba927b0af9f4
korigamik-hypr <korigamik@gmail.com>
2024-01-15 01:43:01 +0530

    fix(nvim): config, which-key and lspconfig

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -7,7 +7,7 @@ local plugins = {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
+      require "custom.configs.lsp"
     end, -- Override to setup mason-lspconfig
   },
 
@@ -120,7 +120,7 @@ local plugins = {
       compile_command = {
         cpp = {
           exec = "g++",
-          args = { "$(FNAME)", "-o", "$(FNOEXT)", "-DKORIGAMIK", "-std=c++20", "-Wall" },
+          args = { "$(FNAME)", "-o", "$(FNOEXT)", "-DKORIGAMIK", "-std=c++20", "-Wall", "-Wpedantic", "-H" },
         },
         haskell = {
           exec = "stack",
@@ -257,13 +257,27 @@ local plugins = {
     end,
   },
 
-  --[[ 
-      [NOTE]: custom config 
-      fix which-key there is no `"` here
-  --]]
   {
     "folke/which-key.nvim",
-    keys = { "<leader>", "<c-r>", "<c-w>", "'", "`", "c", "g" },
+    keys = function()
+      return { "<leader>" }
+    end,
+    opts = {
+      motions = { count = false },
+      plugins = {
+        marks = false, -- shows a list of your marks on ' and `
+        registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        presets = {
+          operators = false, -- adds help for operators like d, y, ...
+          motions = false, -- adds help for motions
+          text_objects = false, -- help for text objects triggered after entering an operator
+          windows = false, -- default bindings on <c-w>
+          nav = true, -- misc bindings to work with windows
+          z = true, -- bindings for folds, spelling and others prefixed with z
+          g = true, -- bindings for prefixed with g
+        },
+      },
+    },
   },
 
   -- AI
