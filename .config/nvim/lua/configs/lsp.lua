a5aa40f47444d57d44c7dad4944a9f471239b09a
korigamik-hypr <korigamik@gmail.com>
2024-05-15 02:00:27 +0530

    fix

--- .config/nvim/lua/configs/lsp.lua
+++ .config/nvim/lua/configs/lsp.lua
@@ -62,7 +62,7 @@ lspconfig.hls.setup {
   },
 }
 
---[[ lspconfig.denols.setup {
+lspconfig.denols.setup {
   on_init = on_init,
   root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
   on_attach = on_attach,
