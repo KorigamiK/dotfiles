aac295a5f76d9ac04d123d69701629df0351db9e
korigamik-hypr <korigamik@gmail.com>
2024-08-12 14:13:41 +0530

    updates

--- .config/nvim/lua/configs/lsp.lua
+++ .config/nvim/lua/configs/lsp.lua
@@ -1,19 +1,21 @@
 local configs = require "nvchad.configs.lspconfig"
 
 configs.defaults()
-
+local handlers = {
+  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+  ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+}
 local on_attach = configs.on_attach
 local on_init = configs.on_init
 local capabilities = configs.capabilities
 
----@diagnostic disable-next-line: different-requires
 local lspconfig = require "lspconfig"
 
--- if you just want default config for the servers then put them in a table
 local servers = { "html", "astro", "jsonls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
+    handlers = handlers,
     on_init = on_init,
     on_attach = on_attach,
     capabilities = capabilities,
@@ -21,6 +23,7 @@ for _, lsp in ipairs(servers) do
 end
 
 lspconfig.tailwindcss.setup {
+  handlers = handlers,
   init_options = {
     userLanguages = { stpl = "html" },
   },
@@ -38,6 +41,7 @@ lspconfig.tailwindcss.setup {
 }
 
 lspconfig.clangd.setup {
+  handlers = handlers,
   on_init = on_init,
   on_attach = on_attach,
   capabilities = capabilities,
@@ -50,6 +54,7 @@ lspconfig.clangd.setup {
 }
 
 lspconfig.hls.setup {
+  handlers = handlers,
   on_init = on_init,
   filetypes = { "haskell", "lhaskell", "cabal" },
   on_attach = on_attach,
@@ -63,6 +68,7 @@ lspconfig.hls.setup {
 }
 
 lspconfig.denols.setup {
+  handlers = handlers,
   on_init = on_init,
   root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
   on_attach = on_attach,
@@ -83,6 +89,7 @@ lspconfig.denols.setup {
 }
 
 lspconfig.tsserver.setup {
+  handlers = handlers,
   autostart = true,
   on_init = on_init,
   root_dir = lspconfig.util.root_pattern "package.json",
@@ -92,29 +99,8 @@ lspconfig.tsserver.setup {
 }
 
 lspconfig.solidity_ls_nomicfoundation.setup {
+  handlers = handlers,
   on_init = on_init,
   on_attach = on_attach,
   capabilities = capabilities,
-}
-
-lspconfig.lua_ls.setup {
-  on_init = on_init,
-  on_attach = on_attach,
-  capabilities = capabilities,
-
-  settings = {
-    Lua = {
-      diagnostics = { globals = { "vim" } },
-      workspace = {
-        library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-          [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
-          [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
-    },
-  },
 }
